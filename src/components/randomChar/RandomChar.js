73bf30c13d1aa331f6ad17018cc5b7444c88152d
oleksandr <dovhoteles.oleksandr@gmail.com>
2023-07-28 00:20:37 +0300

    added check for description length

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -81,28 +81,34 @@ const View = ({char}) => {
     let imgStyle = {'objectFit' : 'cover'};
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
         imgStyle = {'objectFit' : 'contain'};
+}
+
+const cutDescription = (description) => {
+    if (description.length > 150) {
+        return `${description.substring(0, 150)}...`;
     }
+    return description
+}
 
     return (
-    <div className="randomchar__block">
-        <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
-        <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            {description ?
-                <p className="randomchar__descr">{description}</p> : 
-                <p>Ooops... Description is not found!!!</p>
-             }
-            <p className="randomchar__descr">{description}</p>
-            <div className="randomchar__btns">
-                <a href={homepage} className="button button__main">
-                    <div className="inner">homepage</div>
-                </a>
-                <a href={wiki} className="button button__secondary">
-                    <div className="inner">Wiki</div>
-                </a>
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                {description ?
+                    <p className="randomchar__descr">{cutDescription(description)}</p> :
+                    <p>Ooops... Description is not found!!!</p>
+                }
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
