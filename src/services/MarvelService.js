5afad5ced02da910a60f03fa315fd3a80baae69b
oleksandr <dovhoteles.oleksandr@gmail.com>
2023-07-30 00:33:04 +0300

    added characters to the page and their description

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -26,11 +26,13 @@ _apiKey = 'apikey=0dfbdada31096f9121221db3cd3d7d06';
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
             description: char.description,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
